6658c66934c3eca253b52b34db0df516fcc7a455
Ник Скидан <skidannikitas@gmail.com>
2020-06-29 19:27:34 +0600

    Indexator's

---
+++ Practic - indexators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practic___indexators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[100];
+            int rangeStart, rangeEnd;
+            Console.WriteLine("Please,enter you're start of range");
+            rangeStart = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please,enter you're end of range");
+            rangeEnd = int.Parse(Console.ReadLine());
+            RangeOfArray.Meaning(rangeStart, rangeEnd);
+        }
+    }
+}
---
+++ Practic - indexators/RangeOfArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practic___indexators
+{
+    public class RangeOfArray
+    {
+        public static int upRange;
+        public static int downRange;
+
+        private int[] array = new int[upRange - downRange];
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                Check(value,index);
+            }
+        }
+        public void Check(int value,int index)
+        {
+            if(downRange < 0)
+            {
+                array[index - (downRange)] = value;
+            }
+        }
+        public static void Meaning(int rangeStart,int rangeEnd)
+        {
+            downRange = rangeStart;
+            upRange = rangeEnd;
+        }
+    }
+}
